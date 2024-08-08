6870beb7f2011e48b69ec814edcf1ad7cd829a8e
bairen <bairen@wacai.com>
2024-08-08 11:16:15 +0800

    修改 Bootstrap! 注释BootstrapChecks, 因为会报错: : Unknown codebases xxx in policy file

--- server/src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
+++ server/src/main/java/org/elasticsearch/bootstrap/Bootstrap.java
@@ -207,18 +207,18 @@ final class Bootstrap {
         IfConfig.logIfNecessary();
 
         // install SM after natives, shutdown hooks, etc.
-        try {
-            Security.configure(environment, BootstrapSettings.SECURITY_FILTER_BAD_DEFAULTS_SETTING.get(settings));
-        } catch (IOException | NoSuchAlgorithmException e) {
-            throw new BootstrapException(e);
-        }
+//        try {
+//            Security.configure(environment, BootstrapSettings.SECURITY_FILTER_BAD_DEFAULTS_SETTING.get(settings));
+//        } catch (IOException | NoSuchAlgorithmException e) {
+//            throw new BootstrapException(e);
+//        }
 
         node = new Node(environment) {
             @Override
             protected void validateNodeBeforeAcceptingRequests(
                 final BootstrapContext context,
                 final BoundTransportAddress boundTransportAddress, List<BootstrapCheck> checks) throws NodeValidationException {
-                BootstrapChecks.check(context, boundTransportAddress, checks);
+                //BootstrapChecks.check(context, boundTransportAddress, checks);
             }
         };
     }
